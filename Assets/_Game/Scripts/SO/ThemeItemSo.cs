0efa83411be0c07133399903fcb93e926172b7a2
DatTranTuan <145355463+DatTranTuan@users.noreply.github.com>
2024-02-03 02:10:02 +0700

    Add more UI

---
+++ Assets/_Game/Scripts/SO/ThemeItemSo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ThemeItemSo : ScriptableObject
+{
+    public List<ThemeItem> listThemes;
+}
